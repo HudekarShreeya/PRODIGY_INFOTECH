475b7a74baea883fcb58ea793c492b7d33c79186
HudekarShreeya <147175670+HudekarShreeya@users.noreply.github.com>
2024-03-13 20:03:48 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,9 @@
+// Add background color change on scroll
+window.addEventListener('scroll', function() {
+  var navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+  } else {
+    navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+  }
+});
