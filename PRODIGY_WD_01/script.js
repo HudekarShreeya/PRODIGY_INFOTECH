b3bc886eaf87d51c1c7ac2ec22dcef8253989f50
HudekarShreeya <147175670+HudekarShreeya@users.noreply.github.com>
2024-03-20 21:29:48 +0530

    Delete PRODIGY_WD_01 directory

--- PRODIGY_WD_01/script.js
+++
@@ -1,9 +0,0 @@
-// Add background color change on scroll
-window.addEventListener('scroll', function() {
-  var navbar = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-  } else {
-    navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-  }
-});
